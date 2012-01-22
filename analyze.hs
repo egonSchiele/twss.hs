8d069815445765210bfc6561ea6ae79fb4e62ddc
Aditya Bhargava <batman@scribd.com>
2012-01-21 22:37:33 -0800

    remove analyze

--- analyze.hs
+++
@@ -1,5 +0,0 @@
-module Analyze where
-
--- the array used in the foldl has the type [tp, fp, fn, tn]
-getClassificationStats isClass trainingData validationData = foldl () [] validationData
-    where ic = isClass --something
